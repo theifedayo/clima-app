52e71646be89f0e1ea48a8aa1d7a5f43cf88adb6
adesiyan-ifedayo <adesiyanifedayo@gmail.com>
2021-02-20 17:11:03 +0100

    Set Location class to get current position

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +10,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation () async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
+
+
+// body: Center(
+// child: RaisedButton(
+// onPressed: () {
+// getLocation();
+// //Get the current location
+// },
+// child: Text('Get Location'),
+// ),
+// ),
