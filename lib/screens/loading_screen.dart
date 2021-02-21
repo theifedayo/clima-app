c9fa7dfe80fff08d192693b5f4ac1a8398e4bc10
adesiyan-ifedayo <adesiyanifedayo@gmail.com>
2021-02-21 15:02:17 +0100

    Passed data from loading screen to location screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,8 +17,8 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  double latitude;
-  double longitude;
+  // double latitude;
+  // double longitude;
 
   @override
   void initState() {
@@ -33,14 +33,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
       Location location = Location();
       await location.getCurrentLocation();
 
-      latitude = location.latitude;
-      longitude = location.longitude;
-
-      NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+      NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
 
       var weatherData = await networkHelper.getData();
       Navigator.push(context, MaterialPageRoute(builder: (context){
-        return LocationScreen();
+        return LocationScreen(locationWeather: weatherData);
       }));
     }catch(e){
       Navigator.push(context, MaterialPageRoute(builder: (context){
@@ -57,9 +54,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SpinKitWave(
+        child: SpinKitRing(
           color: Colors.white,
-          size: 30.0
+          size: 60.0
         ),
       )
     );
